01e7bf9d1a9649ffb9c827760c9a653bfb065572
Ahmed Melouk <forprogramming8@gmail.com>
2026-04-10 15:23:38 +0200

    feat: add authorization to users#show to restrict access to own profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authorize_creator!, only: %i[show]
   def show
     @user = User.find(params[:id])
+  end
+
+  def authorize_creator!
+    unless User.find(params[:id]) == current_user
+      redirect_to root_path, alert: "You are not authorized to perform this action."
+    end
   end
 end
