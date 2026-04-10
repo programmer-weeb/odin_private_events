3ceae097d0fb60e5cf1ec012cdfc205bb0d48c7a
programmer-weeb <84931803+programmer-weeb@users.noreply.github.com>
2026-04-10 10:48:58 +0200

    feat: expose edit, update, and destroy routes for events

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root "events#index"
 
-  resources :events, only: [:index, :show, :new, :create] do
+  resources :events do
     resources :invitations, only: [:create]
   end
 
