b9b95985e1048053d0393109784a5d919aadd33b
programmer-weeb <84931803+programmer-weeb@users.noreply.github.com>
2026-04-10 10:42:08 +0200

    feat: enforce private event visibility and add privacy toggle UI
    - Filter Events#index to only show public events or events created/invited by current user
    - Restrict Events#show access for private events to authorized users
    - Add privacy checkbox to event creation form
    - Permit 'private' attribute in event parameters

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,12 +2,25 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
 
   def index
-    @past_events = Event.past
-    @upcoming_events = Event.upcoming
+    if user_signed_in?
+      @events = Event.where(private: false)
+                     .or(Event.where(creator_id: current_user.id))
+                     .or(Event.where(id: current_user.invited_events.select(:id)))
+                     .distinct
+    else
+      @events = Event.public_events
+    end
+    
+    @past_events = @events.past
+    @upcoming_events = @events.upcoming
   end
 
   def show
-    @event = Event.includes(:attendees).find(params[:id])
+    @event = Event.includes(:attendees, :invitees).find(params[:id])
+    
+    if @event.private? && @event.creator != current_user && !@event.invitees.include?(current_user)
+      redirect_to root_path, alert: "You are not authorized to view this private event."
+    end
   end
 
   def new
@@ -27,6 +40,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:title, :description, :event_date, :location)
+    params.require(:event).permit(:title, :description, :event_date, :location, :private)
   end
 end
