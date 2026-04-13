1befd2c3d04e7b7ac8d29a98f7bc567989ee3b24
Ahmed Melouk <forprogramming8@gmail.com>
2026-04-13 15:50:23 +0200

    Refactor user profile attendance lookup into controller and reuse loaded user
    Move past/upcoming attended event queries out of users/show view.
    
    - load @user once with set_user before_action
    - assign @past_attended_events and @upcoming_attended_events in show
    - update profile view to render controller ivars only
    - reuse @user in authorize_creator! instead of second User.find

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show]
   before_action :authorize_creator!, only: %i[show]
+
   def show
+    @past_attended_events = @user.attended_events.past
+    @upcoming_attended_events = @user.attended_events.upcoming
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 
   def authorize_creator!
-    unless User.find(params[:id]) == current_user
+    unless @user == current_user
       redirect_to root_path, alert: "You are not authorized to perform this action."
     end
   end
